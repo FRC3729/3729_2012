f64c3ce0a0bd6b91a58f41a8fb8fbe388948f1ff
Matthew Haney <mahaney@q.com>
2012-03-10 13:58:11 -0700

    * Added a NetBeans project to debug drive, since it's been screwy

---
+++ DriveDebug/src/edu/wpi/first/wpilibj/templates/Input.java
@@ -0,0 +1,211 @@
+/*  ______ ______ ______ ______
+ * |__    |      |__    |  __  |
+ * |__    |_     |    __|__    |
+ * |______| |____|______|______|
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Input {
+
+    private Joystick _joy0;
+    private Joystick _joy1;
+    private Joystick _controller;
+    private Joystick _controller2;
+    private int booleanInputs = 0;
+    public static final int mecanum = 0;
+    public static final int arcade_joy = 1;
+    public static final int arcade_controller = 2;
+    public static final int tank = 3;
+    public static final int locked = 4;
+    protected int mode = arcade_controller;
+    // Switch between 1.0 and Params.drive_creep_scale_factor
+    private double scale_factor = 1.0;
+
+    public Input() {
+        this._joy0 = new Joystick(1);
+        this._joy1 = new Joystick(2);
+        this._controller = new Joystick(3);
+        this._controller2 = new Joystick(4);
+    }
+
+    public void set_mode(int m) {
+        this.mode = m;
+    }
+
+    public boolean check_button(int joystick, int button_id) {
+        switch (joystick) {
+            case 0:
+                return this._joy0.getRawButton(button_id);
+            case 1:
+                return this._joy1.getRawButton(button_id);
+            case 2:
+                return this._controller.getRawButton(button_id);
+            case 3:
+                return this._controller2.getRawButton(button_id);
+            default:
+                return check_button(2, button_id);
+        }
+    }
+
+    public int get_mode() {
+        return mode;
+    }
+
+    /*
+     * Modes: 0 = mecanum, 3 joysticks 1 = arcade, 2 joysticks 2 = arcade, 1
+     * joystick 1 controller 3 = tank, 3 joysticks 4 = locked, no input
+     */
+    public double get_x() {
+        switch (mode) {
+            case mecanum:
+                return Utility.normalize(this._joy1.getRawAxis(1), -1.0, 1.0);
+            case arcade_joy:
+                return Utility.expo(Utility.normalize(this._joy1.getX(), -1.0, 1.0), Params.JOYEXPO);
+            case arcade_controller:
+                return Utility.expo(Utility.normalize(this._controller.getX(), Params.XMIN, Params.XMAX), Params.XEXPO) * -1.0;
+            case tank:
+                return Utility.expo(Utility.normalize(this._joy0.getY(), -1.0, 1.0) * -1.0, Params.JOYEXPO);
+            case locked:
+                return 0;
+            default:
+                mode = 2;
+                return get_x();
+        }
+    }
+
+    public double get_y() {
+        switch (mode) {
+            case mecanum:
+                return Utility.normalize(-this._joy0.getRawAxis(2), -1.0, 1.0);
+            case arcade_joy:
+                return Utility.expo(Utility.normalize(this._joy1.getY(), -1.0, 1.0) * -1.0, Params.JOYEXPO);
+            case arcade_controller:
+                return Utility.expo(Utility.normalize(this._controller.getY(), Params.YMIN, Params.YMAX), Params.YEXPO);
+            case tank:
+                return Utility.expo(Utility.normalize(this._joy1.getY(), -1.0, 1.0) * -1.0, Params.JOYEXPO);
+            case locked:
+                return 0;
+            default:
+                mode = 2;
+                return get_y();
+        }
+    }
+
+    public double get_z() {
+        switch (mode) {
+            case mecanum:
+                return Utility.normalize(this._joy0.getRawAxis(1), -1.0, 1.0);
+            case arcade_joy:
+                return Utility.expo(Utility.normalize(this._joy0.getX(), -1.0, 1.0), Params.JOYEXPO);
+            case arcade_controller:
+                return Utility.expo(Utility.normalize(this._controller.getZ(), Params.ZMIN, Params.ZMAX), Params.YEXPO);
+            case tank:
+                return Utility.expo(Utility.normalize(this._joy0.getX(), -1.0, 1.0), Params.JOYEXPO);
+            case locked:
+                return 0;
+            default:
+                mode = 2;
+                return get_z();
+        }
+    }
+
+    public double get_w() {
+        switch (mode) {
+            case mecanum:
+                return Utility.normalize(this._joy1.getRawAxis(2), -1.0, 1.0);
+            case arcade_joy:
+                return Utility.expo(Utility.normalize(this._joy0.getY(), -1.0, 1.0) * -1.0, Params.JOYEXPO);
+            case arcade_controller:
+                return Utility.expo(Utility.normalize(this._controller.getTwist(), Params.ROTMIN, Params.ROTMAX) * -1.0, Params.ROTEXPO);
+            case tank:
+                return Utility.expo(Utility.normalize(this._joy1.getX(), -1.0, 1.0), Params.JOYEXPO);
+            case locked:
+                return 0;
+            default:
+                mode = 2;
+                return get_w();
+        }
+    }
+
+    public double get_throttle(int joy) {
+        switch (mode) {
+            case mecanum:
+            case arcade_joy:
+                switch (joy) {
+                    case 0:
+                        return this._joy0.getThrottle();
+                    case 1:
+                        return this._joy1.getThrottle();
+                    default:
+                        joy = 0;
+                        return get_throttle(joy);
+                }
+            case arcade_controller:
+                return this._joy0.getThrottle();
+            case tank:
+                switch (joy) {
+                    case 0:
+                        return this._joy0.getThrottle();
+                    case 1:
+                        return this._joy1.getThrottle();
+                    case 2:
+                        return this._controller.getThrottle();  // 'Controller' is actually 3rd joystick
+                    case 3:
+                        return this._controller2.getThrottle();
+                    default:
+                        joy = 0;
+                        return get_throttle(joy);
+                }
+            case locked:
+                return 0;
+            default:
+                mode = 2;
+                return get_throttle(0);
+        }
+    }
+
+    public double get_throttle() {
+        return get_throttle(0);
+    }
+
+    public double get_twist(int joy) {
+        switch (mode) {
+            case mecanum:
+            case arcade_joy:
+                switch (joy) {
+                    case 0:
+                        return this._joy0.getTwist();
+                    case 1:
+                        return this._joy1.getTwist();
+                    default:
+                        return get_twist(0);
+                }
+            case arcade_controller:
+                return this._joy0.getTwist();
+            case tank:
+                switch (joy) {
+                    case 0:
+                        return this._joy0.getTwist();
+                    case 1:
+                        return this._joy1.getTwist();
+                    case 2:
+                        return this._controller.getTwist();  // 'Controller' is actually 3rd joystick
+                    case 3:
+                        return this._controller2.getTwist();
+                    default:
+                        return get_twist(0);
+                }
+            case locked:
+                return 0;
+            default:
+                mode = 2;
+                return get_twist(0);
+        }
+    }
+
+    public double get_twist() {
+        return get_twist(0);
+    }
+}
---
+++ DriveDebug/src/edu/wpi/first/wpilibj/templates/Utility.java
@@ -0,0 +1,74 @@
+/*  ______ ______ ______ ______
+ * |__    |      |__    |  __  |
+ * |__    |_     |    __|__    |
+ * |______| |____|______|______|
+ */
+package edu.wpi.first.wpilibj.templates;
+
+public class Utility {
+
+    /**
+     * @brief Clamp a value between a minimum and maximum
+     *
+     * @author Morgan Jones
+     * @param value The value to clamp
+     * @param min The minimum value to allow
+     * @param max The maximum value to allow
+     * @return The clamped value, in [min, max]
+     */
+    public static double clamp(double value, double min, double max) {
+        if (value < min) {
+            return min;
+        } else if (value > max) {
+            return max;
+        }
+        return value;
+    }
+
+    /**
+     * @brief Normalize input between -1.0 and 1.0. Also take into account min
+     * and max values given by controller.
+     *
+     * @author Adam Bryant
+     * @param val Input value to be normalized
+     * @param min Minimum controller value. If using a joystick, supply -1.0.
+     * @param max Maximum controller value. If using a joystick, supply 1.0.
+     * @return The normalized value
+     */
+    public static double normalize(double val, double min, double max) {
+        double retVal = 0.0;
+        if (val < 0.0) {
+            retVal = Math.abs(val) / min;
+        } else if (val > 0.0) {
+            retVal = Math.abs(val) / max;
+        }
+        return clamp(val, -1.0, 1.0);
+    }
+
+    /**
+     * @brief Convert a boolean to an integer
+     *
+     * @param val The boolean to convert
+     * @return An integer-ized boolean
+     */
+    public static int to_int(boolean val) {
+        if (val) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * @brief Exponential function that makes the controller more sensitive
+     * toward the center and less so toward the outside
+     *
+     * @author Adam Bryant
+     * @param x Input to be expo-ed
+     * @param a Pre-defined exponentiation factor
+     * @return The expo-ed value
+     */
+    public static double expo(double x, double a) {
+        return (a * (x * x * x) + (1 - a) * x);
+    }
+}
